2f68fc99496d761b8f2f438a4a91ad2bb2c26450
Lobzev Nikita <lobzev.n@yandex.ru>
2024-04-21 22:23:01 +0300

    Fixed the mistakes

--- app/src/main/java/com/example/fragments/FirstFragment.kt
+++ app/src/main/java/com/example/fragments/FirstFragment.kt
@@ -8,7 +8,6 @@ import androidx.fragment.app.Fragment
 
 class FirstFragment : Fragment() {
 
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
@@ -16,8 +15,7 @@ class FirstFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_first, container, false)
     }
 
-    companion object FragmentOne {
-
+    companion object {
         @JvmStatic
         fun newInstance() = FirstFragment()
     }
--- app/src/main/java/com/example/fragments/MainActivity.kt
+++ app/src/main/java/com/example/fragments/MainActivity.kt
@@ -6,23 +6,22 @@ import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
+
+    fun goToFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_home, FirstFragment.newInstance()).commit()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-        val image: ImageView = findViewById(R.id.picture)
-        val clickMe: Button = findViewById(R.id.ClickMe)
-        val firstPageText: TextView = findViewById(R.id.first_page_text)
-
+        val clickMe: Button = findViewById(R.id.clickMe)
         clickMe.setOnClickListener {
-            val intent = Intent(this, SecondActivity::class.java)
-            startActivity(intent)
-
-
+            goToFragment(fragment = Fragment())
         }
-
     }
 }
